74a52f6c1fa924e5145d9da8906a02f7b92ecd87
Daniel Pettifer <danielpettifer@me.com>
2018-10-12 11:11:43 +0100

    Static app includes TimerDashboard and EditableTimerList components

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,37 @@
+class TimersDashboard extends React.Component {
+    render () {
+        return (
+            <div className="ui three column centered grid">
+                <div className="column">
+                    <EditableTimerList />
+                    <ToggleableTimerForm
+                        isOpen={true}
+                        />
+                </div>
+            </div>
+        );
+    }
+}
 
+class EditableTimerList extends React.Component {
+    render () {
+        return (
+            <div id="timers">
+                <EditableTimer
+                    title="Learn React"
+                    project="World domination"
+                    elapsed="9769769876"
+                    runningSince={null}
+                    editFormOpen={false}
+                />
+                <EditableTimer
+                    title="Learn Extreme Tea making"
+                    project="World domination"
+                    elapsed="658658658765"
+                    runningSince={null}
+                    editFormOpen={true}
+                />
+            </div>
+        );
+    }
+}
